a746ebae1d453f01f9c0b2db6851ef66885382d7
mankamolnar <markmanomolnar@gmail.com>
2017-05-04 10:24:47 +0200

    fixed all bugs, deleted abstract class and implemented interface

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,11 +1,19 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.Date;
+
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.interfaces.*;
+import spark.Request;
 
 /**
  * Created by flowerpower on 2017. 05. 02..
  */
-public class Cart extends AbstractCart {
+public class Cart implements Sessionable {
+
+    public Order order;
+    public ArrayList<LineItem> lineItem;
 
     public Cart() {
         this.order = new Order(0, "");
@@ -14,7 +22,6 @@ public class Cart extends AbstractCart {
 
     public void addToCart(Product product, int quantity) {
         if (isProductInCart(product)) {
-            System.out.println("bejött");
             modifyItem(product, quantity);
         } else {
             lineItem.add(new LineItem(product, quantity, product.getDefaultPrice()));
@@ -41,7 +48,70 @@ public class Cart extends AbstractCart {
        return lineItem.get(index);
     }
 
+    public void initFromSession(Request request) {
+        initOrderFromSession(request);
+        initLineItemsFromSession(request);
+
+    }
+
+    public void saveToSession(Request request) {
+        saveOrderToSession(request);
+        saveProductsToSession(request);
+
+    }
+
     // *** AID METHODS ***
+    public void initOrderFromSession(Request request) {
+        if (request.session().attribute("order") != null) {
+            String[] sessionValues = request.session().attribute("order").toString().split(";");
+
+            Date orderDate = new Date();
+            orderDate.setTime(Long.parseLong(sessionValues[3]));
+
+            order = new Order(
+                    Integer.parseInt(sessionValues[0]),
+                    Integer.parseInt(sessionValues[1]),
+                    sessionValues[2],
+                    orderDate,
+                    sessionValues[4]
+            );
+
+        }
+    }
+
+    public void initLineItemsFromSession(Request request) {
+        if (request.session().attribute("lineitem") != null) {
+            String[] products = request.session().attribute("lineitem").toString().split(";");
+
+            for (int i = 0; i < products.length; i++) {
+                String[] tmpProduct = products[i].split(",");
+
+                lineItem.add(new LineItem(
+                        ProductDaoMem.getInstance().find(Integer.parseInt(tmpProduct[0])),
+                        Integer.parseInt(tmpProduct[1]),
+                        Float.parseFloat(tmpProduct[2])
+                ));
+            }
+
+        }
+    }
+
+    public void saveOrderToSession(Request request) {
+        request.session().attribute("order", order);
+    }
+
+    public void saveProductsToSession(Request request) {
+        String sessionValue = "";
+
+        for (int i = 0; i < lineItem.size(); i++) {
+            sessionValue += lineItem.get(i) + ";";
+        }
+
+        if (sessionValue.trim() != "") {
+            request.session().attribute("lineitem", sessionValue.substring(0, sessionValue.length()-1));
+        }
+    }
+
     public boolean isProductInCart(Product product) {
         for (int i = 0; i < lineItem.size(); i++) {
             if (lineItem.get(i).product == product) {
