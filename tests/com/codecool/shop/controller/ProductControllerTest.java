d10d1c69d19ef6d4d99db6f04b168d6a5bcfd72b
bpstrngr <bpstrngr@gmail.com>
2017-05-18 23:51:42 +0200

    one successful test

---
+++ tests/com/codecool/shop/controller/ProductControllerTest.java
@@ -0,0 +1,15 @@
+package com.codecool.shop.controller;
+
+import org.junit.jupiter.api.Test;
+import spark.Response;
+import sun.misc.Request;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void testParameterValidity() {
+    }
+
+}
