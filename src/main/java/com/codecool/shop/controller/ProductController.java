71baab14988d3d41b7f13fb2025c9eed3984d458
Balázs Máté <balazsm86@gmail.com>
2017-05-22 14:38:22 +0200

    Logging ProductController class

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.Cart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.ModelAndView;
@@ -14,12 +16,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     public static ModelAndView renderProducts(Request req, Response res) {
-//        ProductDao productDataStore = ProductDaoMem.getInstance();
-//        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-
+        logger.debug("Entering renderProducts(req={}, res={})", req, res);
         ProductDao productDataStore = ProductDaoJdbc.getInstance();
         SupplierDao supplierDataStore = SupplierDaoJdbc.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
@@ -31,15 +31,20 @@ public class ProductController {
         params.put("categories", productCategoryDataStore.getAll());
         params.put("suppliers", supplierDataStore.getAll());
         if (req.queryParams("category") != null) {
+            logger.debug("Filtering products by category");
             params.put("filter", productCategoryDataStore.find(Integer.parseInt(req.queryParams("category"))));
             params.put("products", productDataStore.getBy(productCategoryDataStore.find(Integer.parseInt(req.queryParams("category")))));
         } else if (req.queryParams("supplier") != null) {
+            logger.debug("Filtering products by supplier");
             params.put("filter", supplierDataStore.find(Integer.parseInt(req.queryParams("supplier"))));
             params.put("products", productDataStore.getBy(supplierDataStore.find(Integer.parseInt(req.queryParams("supplier")))));
         } else {
+            logger.debug("No filtering products");
             params.put("filter", productCategoryDataStore.find(1));
             params.put("products", productDataStore.getAll());
         }
+        logger.debug("Params length: {}", params.size());
+        logger.debug("Leaving renderProducts()");
         return new ModelAndView(params, "product/index");
     }
 
