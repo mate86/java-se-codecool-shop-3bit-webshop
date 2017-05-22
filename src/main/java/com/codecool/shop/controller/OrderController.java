1db1963b6e65d8e786c95939ee1aebfd655c86f9
Balázs Máté <balazsm86@gmail.com>
2017-05-22 14:56:48 +0200

    Logging OrderController class

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.implementation.OrderDaoJdbc;
 import com.codecool.shop.model.Cart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -13,16 +15,21 @@ import java.util.Map;
  * Created by flowerpower on 2017. 05. 04..
  */
 public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     public static ModelAndView checkout(Request req, Response res) {
+        logger.debug("Entering checkout(req={}, res={})", req, res);
         Map params = new HashMap<>();
         Cart cart = new Cart();
         cart.initFromSession(req);
         params.put("cart", cart);
-
+        logger.debug("Params length: {}", params.size());
+        logger.debug("Leaving checkout()");
         return new ModelAndView(params, "order/checkout");
     }
 
     public static ModelAndView payment(Request req, Response res) {
+        logger.debug("Entering payment(req={}, res={})", req, res);
 
         //Instantiate needed variables
         Map params = new HashMap<>();
@@ -30,6 +37,7 @@ public class OrderController {
         cart.initFromSession(req);
 
         //Set changings on order and save
+        logger.debug("Setting order's user");
         cart.order.setByUser(
                 req.queryParams("name"),
                 req.queryParams("email"),
@@ -39,14 +47,18 @@ public class OrderController {
                 req.queryParams("description")
         );
         cart.order.setStatus(1);
+        logger.debug("Setting order's status");
         cart.saveToSession(req);
+        logger.debug("Cart saved to session");
 
         params.put("cart", cart);
-
+        logger.debug("Params length: {}", params.size());
+        logger.debug("Leaving payment()");
         return new ModelAndView(params, "order/payment");
     }
 
     public static ModelAndView confirmation(Request req, Response res) {
+        logger.debug("Entering confirmation(req={}, res={})", req, res);
 
         //Instantiate needed variables
         Map params = new HashMap<>();
@@ -54,7 +66,9 @@ public class OrderController {
         cart.initFromSession(req);
 
         //Set changings on order and save
+        logger.debug("Setting order's payment method");
         cart.order.setPaymentMethod(Integer.parseInt(req.queryParams("payment")));
+        logger.debug("Setting order's status");
         cart.order.setStatus(2);
         cart.dropSession(req.session());
         params.put("cart", cart);
@@ -70,7 +84,8 @@ public class OrderController {
                 cart.order.getPaymentMethod().getId(),
                 cart.order.getStatus()
         );
+        logger.debug("Params length: {}", params.size());
+        logger.debug("Leaving confirmation()");
         return new ModelAndView(params, "order/confirmation");
     }
-
 }
